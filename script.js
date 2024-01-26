f62ed6e0abc8818ba46782eef64ba0283e6ded03
Orbter <assouline39@gmail.com>
2024-01-26 17:14:54 +0200

    started to make the creating a card

--- script.js
+++ script.js
@@ -27,21 +27,52 @@ overlay.addEventListener("click", function (event) {
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  createObject();
+  console.log(title);
+  console.log(myLibrary);
+  console.log(book1);
+});
+//const book1 = new Book("Game Of Thrones", "George R. R. Martin", "694", true);
+
+function createObject() {
   const title = document.getElementById("form-title").value;
   const author = document.getElementById("form-author").value;
   const pages = document.getElementById("form-pages").value;
   const read = document.getElementById("read-checkbox").value;
   const book1 = new Book(title, author, pages, read);
   myLibrary.push(book1);
-  console.log(title);
-  console.log(myLibrary);
-});
-//const book1 = new Book("Game Of Thrones", "George R. R. Martin", "694", true);
-
-function addBookToLibrary() {
-  const newDIv = document.createElement("div");
-  newDIv.classList.add("card");
-  bookCard.appendChild(newDIv);
 }
 
-addBookToLibrary();
+function createCard() {
+  const newCard = document.createElement("div");
+  const cardTitle = document.createElement("div");
+  const cardAuthor = document.createElement("div");
+  const cardPages = document.createElement("div");
+  const cardButtons = document.createElement("div");
+  const titleH3 = document.createElement("h3");
+  const authorP = document.createElement("p");
+  const pagesP = document.createElement("p");
+  const readButton = document.createElement("button");
+  const removeCard = document.createElement("button");
+  //appending all the dives and elements
+  newCard.appendChild(cardTitle);
+  newCard.appendChild(cardAuthor);
+  newCard.appendChild(cardPages);
+  newCard.appendChild(cardButtons);
+  cardTitle.appendChild(titleH3);
+  cardAuthor.appendChild(authorP);
+  cardPages.appendChild(pagesP);
+  cardButtons.appendChild(readButton);
+  cardButtons.appendChild(removeCard);
+  //adding classes
+  newCard.classList.add("card", "flex");
+  readButton.classList("card-read");
+  removeCard.classList("remove");
+  //adding the text
+  readButton.innerText = "Read";
+  removeCard.innerText = "Remove";
+}
+
+function addBookToLibrary(card) {
+  bookCard.appendChild(card);
+}
