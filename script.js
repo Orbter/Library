e7fb7b6b6bc3131f21e637462fa0521b38bfe212
Orbter <assouline39@gmail.com>
2024-01-26 14:51:49 +0200

    update to see the form visibility

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const myLibrary = [];
-
+const form = document.querySelector(".form");
+const addBook = document.querySelector(".add");
+const overlay = document.createElement("div");
+overlay.classList.add("overlay");
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
-const book1 = new Book("Game Of Thrones", "George R. R. Martin", "694", true);
+addBook.addEventListener("click", function (event) {
+  if (form.classList.contains("invisible")) {
+    form.classList.remove("invisible");
+    overlay.classList.add("overlay");
+
+    document.body.appendChild(overlay);
+  }
+});
+overlay.addEventListener("click", function (event) {
+  if (!form.classList.contains("invisible")) {
+    form.classList.add("invisible");
+    overlay.classList.remove("overlay");
+  }
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const title = document.getElementById("form-title");
+  const author = document.getElementById("form-author");
+  const pages = document.getElementById("form-pages");
+  const read = document.getElementById("read-checkbox");
+  const book1 = new Book(title, author, pages, read);
+  myLibrary.push(book1);
+});
+//const book1 = new Book("Game Of Thrones", "George R. R. Martin", "694", true);
+
 const book2 = new Book("The Hobbit", "John Ronald Reuel Tolkien", "320", true);
 function addBookToLibrary() {
-  myLibrary.push(book1);
   myLibrary.push(book2);
 
   console.log(myLibrary);
